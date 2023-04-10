87e413814eab76595cbac2ff2a6e1e47c349d90b
GauravAsane <119696890+GauravAsane@users.noreply.github.com>
2023-04-10 23:40:28 +0530

    Add files via upload

---
+++ src_Ass6Q2/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment_6';
+
+  public str : String = "Marvellous Infosystems";
+}
---
+++ src_Ass6Q3/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment_6';
+
+  public str : string = "Marvellous Infosystems";
+
+  public MyClass  = "success";
+
+  public setcolor : string = "blue";
+}
